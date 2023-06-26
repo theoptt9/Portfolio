096873b2d3a041c45131e0e55ad43285adbfea43
theoptt9 <test.svt36@gmail.com>
2023-06-26 19:13:03 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocialNetworkComponent } from './social-network/social-network.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SocialNetworkComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/social-network/social-network.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-social-network',
+  templateUrl: './social-network.component.html',
+  styleUrls: ['./social-network.component.scss']
+})
+export class SocialNetworkComponent {
+
+}
