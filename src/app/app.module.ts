8295935e2f7e7f3c85ef0f7d99fc19475f0ac340
theoptt9 <test.svt36@gmail.com>
2023-06-29 15:49:51 +0200

    Adding animation on menu link click

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SocialNetworkComponent } from './social-network/social-network.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { StoriesComponent } from './stories/stories.component';
+import { ProjectComponent } from './project/project.component';
+import { ContactComponent } from './contact/contact.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SocialNetworkComponent
+    SocialNetworkComponent,
+    HomepageComponent,
+    NavigationComponent,
+    StoriesComponent,
+    ProjectComponent,
+    ContactComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent {
+  scrollToSection(sectionId: string): void {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+}
